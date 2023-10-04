2845fae47f83b4ba5d89c907c176a5cc1eaca409
Jacimarct <143716758+Jacimarct@users.noreply.github.com>
2023-10-04 17:12:20 -0300

    20231004 17:12 Jacimar - corrigi o formulário sobre_nós falta muito ainda

--- Arq_CSS/Envio_contato.js
+++ Arq_CSS/Envio_contato.js
@@ -6,11 +6,7 @@ const mensagemConfirmacao = document.getElementById("mensagem-confirmacao");
 formulario.addEventListener("submit", function (event) {
   // Impede o envio padrão do formulário
   event.preventDefault();
-<<<<<<< HEAD
 //aqui alteramos o js
-=======
-
->>>>>>> df2bd56f648eff9d3f77fbc1c06bd5703d755031
   // Aqui você pode adicionar lógica para enviar o formulário por e-mail (se necessário).
 
   // Mostra a mensagem de confirmação
@@ -22,11 +18,7 @@ formulario.addEventListener("submit", function (event) {
                 mensagemEnviado.style.display = "block"; // Exibe a mensagem de confirmação
             }, 1000); // Aguarda 1 segundo (você pode ajustar o tempo conforme necessário)
 
-<<<<<<< HEAD
 
  // Limpa os campos do formulário
-=======
-  // Limpa os campos do formulário
->>>>>>> df2bd56f648eff9d3f77fbc1c06bd5703d755031
   formulario.reset();
 });
