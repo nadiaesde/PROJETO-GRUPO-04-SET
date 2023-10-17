10879a85d72d72940fce414c0e623bb828e66db2
Jacimarct <143716758+Jacimarct@users.noreply.github.com>
2023-10-17 12:58:03 -0300

    20231017 13:00 jacimar

--- Trabalhe_conosco/meu_javascript.js
+++ Trabalhe_conosco/meu_javascript.js
@@ -1,9 +1,10 @@
 // Obtém os elementos do DOM
 const formularioContato = document.getElementById("formulario-colaborador");
 const mostrarFormularioContato = document.getElementById("mostrarFormularioColaborador");
+//const botaoCancelar = document.querySelector(".btn-cancelar")
 
 // Adiciona um evento de clique ao link "Contato"
-mostrarFormularioContato.addEventListener("click", function () {
+mostrarFormularioContato.addEventListener("click", function () { //controladorModal() {
     // Verifica o estado atual do formulário e alterna entre mostrar e ocultar
     if (formularioContato.style.display === "none" || formularioContato.style.display === "") {
         formularioContato.style.display = "block";
@@ -11,3 +12,7 @@ mostrarFormularioContato.addEventListener("click", function () {
         formularioContato.style.display = "none";
     }
 });
+
+// mostrarFormularioContato.addEventListener("click", controladorModal );
+// botaoCancelar.addEventListener("click", controladorModal) 
+
