fc87cf461517f6b865d8bfb5b9a997ed17e945f7
nadiaesde <nadiaesde@gmail.com>
2023-10-30 16:20:41 -0300

    atual 30.10.23

--- Nossos servicos/java_script/trabalhe_conosco.js
+++ Nossos servicos/java_script/trabalhe_conosco.js
@@ -1,16 +1,16 @@
 // Obtém os elementos do DOM
-const formularioContato = document.getElementById("formulario-modal");
-const mostrarFormularioContato = document.getElementById("mostrarFormularioColaborador");
-const botaoCancelar = document.querySelector(".btn-cancelar")
+const formularioColaborador = document.getElementById("formulario-colaborador");
+const mostrarFormularioColaborador = document.getElementById("mostrarFormularioColaborador");
+
 // Adiciona um evento de clique ao link "Contato"
-function controladorModal() {
+mostrarFormularioContato.addEventListener("click", function () {
     // Verifica o estado atual do formulário e alterna entre mostrar e ocultar
     if (formularioContato.style.display === "none" || formularioContato.style.display === "") {
         formularioContato.style.display = "block";
     } else {
         formularioContato.style.display = "none";
     }
-}
+});
 mostrarFormularioContato.addEventListener("click", controladorModal );
 botaoCancelar.addEventListener("click", controladorModal) 
 
@@ -36,4 +36,18 @@ function modalServicos(){
     }
 }
 modalServicos()
+
+// Obtém os elementos do DOM
+const formularioContato = document.getElementById("formulario-modal");
+const mostrarFormularioContato = document.getElementById("mostrarFormularioColaborador");
+const botaoCancelar = document.querySelector(".btn-cancelar")
+// Adiciona um evento de clique ao link "Contato"
+function controladorModal() {
+    // Verifica o estado atual do formulário e alterna entre mostrar e ocultar
+    if (formularioContato.style.display === "none" || formularioContato.style.display === "") {
+        formularioContato.style.display = "block";
+    } else {
+        formularioContato.style.display = "none";
+    }
+};
 
