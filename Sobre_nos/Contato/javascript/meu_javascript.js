a9ce35c66c9b58a8c172cc7d26af1bedd2ebc359
Jacimarct <143716758+Jacimarct@users.noreply.github.com>
2023-11-01 14:03:35 -0300

    20231101 14:10 - Jacimar, mais alguns pequenos ajustes e ideias

--- Sobre_nos/Contato/javascript/meu_javascript.js
+++ Sobre_nos/Contato/javascript/meu_javascript.js
@@ -1,14 +1,33 @@
-const formularioContato_c = document.getElementById("formulario-contato_c");
-const mostrarFormularioContato_c = document.getElementById("mostrarFormularioContato_c");
 
-// Adiciona um evento de clique ao link "Contato"
-mostrarFormularioContato_c.addEventListener("click", function () { //controladorModal() {
-    console.log("o evento de click foi acionado!")
-    // Verifica o estado atual do formulário e alterna entre mostrar e ocultar
-    if (formularioContato_c.style.display === "none" || formularioContato_c.style.display === "") {
-        formularioContato_c.style.display = "block";
-        formularioContato_c.style.zIndex = "999";
+function alternarFormularioContato() {
+    const menuTexto = document.getElementById("menu-texto");
+    const formularioContato = document.getElementById("formulario-contato_c");
+
+    if (formularioContato.style.display === "none" || formularioContato.style.display === "") {
+        // Se o formulário estiver oculto, mostra o formulário e altera o texto do menu
+        formularioContato.style.display = "block";
+        formularioContato.style.zIndex = "999";        
+        menuTexto.textContent = "FECHE AQUI";
+        menuTexto.classList.add("cor-expressiva");
     } else {
-        formularioContato_c.style.display = "none";
+        // Se o formulário estiver visível, esconde o formulário e volta o texto do menu
+        formularioContato.style.display = "none";
+        menuTexto.textContent = "CONTATO";
+        menuTexto.classList.remove("cor-expressiva");        
     }
-});
+//    const formularioContato_c = document.getElementById("formulario-contato_c");
+    const mostrarFormularioContato_c = document.getElementById("mostrarFormularioContato_c");
+    
+    // Adiciona um evento de clique ao link "Contato"
+    mostrarFormularioContato_c.addEventListener("click", function () { //controladorModal() {
+        //console.log("o evento de click foi acionado!")
+        // Verifica o estado atual do formulário e alterna entre mostrar e ocultar
+        if (formularioContato_c.style.display === "none" || formularioContato_c.style.display === "") {
+            formularioContato_c.style.display = "block";
+            formularioContato_c.style.zIndex = "999";
+        } else {
+            formularioContato_c.style.display = "none";
+        }
+    });
+
+}
