2dd4b066dc46d15b89422def3cd373cd32a9501a
Jacimarct <143716758+Jacimarct@users.noreply.github.com>
2023-11-01 16:29:18 -0300

    20231101 16:60 Jacimar - pequena correção

--- Agendamento/javascript/Esqueceu.js
+++ Agendamento/javascript/Esqueceu.js
@@ -1,39 +1,3 @@
-// // Dicionário simulando usuários (usuário, senha, CPF)
-// var usuarios = {
-//     "joao": { "senha": "senha123", "cpf": "12345678900" },
-//     "maria": { "senha": "senha456", "cpf": "98765432100" },
-// };
-
-// function resetPassword() {
-//     var username = document.getElementById("username").value;
-//     var cpf = document.getElementById("cpf").value;
-
-//     if (usuarios[username] && usuarios[username].cpf === cpf) {
-//         // Gerar uma nova senha temporária
-//         var new_password = generatePassword();
-
-//         // Simular o envio da nova senha em um pop-up
-//         alert(username + ": sua nova senha é: " + new_password);
-
-//     } else {
-//         alert("Usuário não encontrado ou CPF incorreto.");
-//     }
-// }
-
-// function generatePassword() {
-//     var length = 8;
-//     var charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-//     var newPassword = "";
-
-//     for (var i = 0; i < length; i++) {
-//         newPassword += charset.charAt(Math.floor(Math.random() * charset.length));
-//     }
-
-//     return newPassword;
-// }
-
-
-
 // Dicionário simulando usuários (usuário, senha, CPF, CNPJ)
 var usuarios = {
     "joao": { "senha": "senha123", "cpf": "12345678900", "cnpj": "12345678000101" },
@@ -50,6 +14,7 @@ function resetPassword() {
 
         // Simular o envio da nova senha em um pop-up
         alert("Sua nova senha é: " + new_password);
+        window.location.href = "Login.html"; // Substitua "pagina_de_login.html" pelo URL real da sua página de login.        
     } else {
         alert("Usuário não encontrado, CPF ou CNPJ incorretos.");
     }
